f08e0ceebb179f23274f676ede283dd5fc189e6b
Luke Boydstun <luke_boydstun1@baylor.edu>
2023-01-30 17:34:50 -0600

    update

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -6,12 +6,31 @@ out_file = open("salesreport.csv", "w")
 data = csv.reader(in_file, delimiter=',')
 
 count = 0
+transactions = []
 
 for row in data:
     if count != 0:
+        id = row[0]
         total = float(row[3])+float(row[4])+float(row[5])
-        line = row[0] + ' ' + str(total) + '\n'
-        out_file.write(line)
+        transaction = [int(id), total]
+        transactions.append(transaction)
     count += 1
+
+entries = len(transactions)
+
+id_total = 0
+id = 250
+
+for i in transactions:
+
+    if i[0] == id:
+        id_total += float(i[1])
+
+    else:
+        line = str(id) + ' ' + str(id_total) + '\n'
+        out_file.write(line)
+        id_total = float(i[1])
+        id += 1
+
 
 out_file.close()
