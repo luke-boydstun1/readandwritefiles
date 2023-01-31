a17c4a81546fcb480d723f0474f0f60d41ebe65b
Luke Boydstun <luke_boydstun1@baylor.edu>
2023-01-30 23:07:23 -0600

    update

--- employee_bonus.py
+++ employee_bonus.py
@@ -4,8 +4,11 @@ in_file = open("EmployeePay.csv", "r")
 
 data = csv.reader(in_file, delimiter=',')
 
+count = 0
 for row in data:
-    line = ''
-    for i in row:
-        line += i + ' '
-    print(line)
+    if count != 0:
+        amount = float(row[3]) * (1+float(row[4]))
+
+        line = row[1] + ' ' + row[2] + ": " + str(amount)
+        print(line)
+    count += 1
